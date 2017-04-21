85bcd35ff62e58d0db2b36f65edfe4a3c0caf3e8
gxe1157 <gx1157@yahoo.com>
2017-04-21 14:28:00 -0400

    building admin panel

---
+++ application/modules/store_items/views/manage.php
@@ -0,0 +1,13 @@
+<h2>Manage Items</h2>
+
+<h3> button here........</h3>
+
+<table class="table table-striped table-bordered">
+  <tr>
+    <td>item1</td>
+    <td>item2</td>
+    <td>item3</td>
+    <td>item4</td>
+    <td>item5</td>
+  </tr>
+</table>
--- application/modules/templates/controllers/templates.php
+++ application/modules/templates/controllers/templates.php
@@ -5,21 +5,20 @@ class Templates extends MX_Controller
 
 function __construct() {
     parent::__construct();
-    $this->load->module('lib');
-    $this->load->module('nav_menu');
-    
+    // $this->load->module('lib');
+
     // echo $this->uri->segment(1).' | '; // controller
     // echo $this->uri->segment(2).' | '; // action
     // echo $this->uri->segment(3).' | '; // 1stsegment
     // echo $this->uri->segment(4).' | '; // 2ndsegment
     // echo $this->uri->segment(5).' | '; // 3ndsegment
-    // echo $this->uri->segment(6); // 4ndsegment        
+    // echo $this->uri->segment(6); // 4ndsegment
 }
 
-function _inner_nav_options( $nav_to = null, $nav) { 
+function _inner_nav_options( $nav_to = null, $nav) {
     $menu_title = urldecode($nav_to);
-    $this->lib->checkField($menu_title,1);    
-    $this->lib->checkArray( $nav['About Us'], 0);    
+    $this->lib->checkField($menu_title,1);
+    $this->lib->checkArray( $nav['About Us'], 0);
 
     $menu_items = $nav[ $menu_title ];
     $menu_view_items = '';
@@ -29,19 +28,22 @@ function _inner_nav_options( $nav_to = null, $nav) {
             <a style="color: blue;" href="'.$menu_items[$lines][1].'" >
                 '.$menu_items[$lines][0].'</a>
           </div>';
-    }   
+    }
 
     echo $menu_view_items;
     die();
 }
 
 function public_main( $page = null, $nav_to = null, $img_dir = null, $page_no = null ) {
+    $this->load->module('lib');
+    $this->load->module('nav_menu');
+
     // get images from directory`
     $imgDir   = 'public/images/'.$img_dir;
     $data = '';
 
     $nav  = $this->nav_menu->index();
-    // if( isset($nav_to) ) $this->_inner_nav_options( $nav_to, $nav );        
+    // if( isset($nav_to) ) $this->_inner_nav_options( $nav_to, $nav );
 
     $data['nav']   = $this->nav_menu->index();
     $data['title'] = 'New Jersey Law Enforcement Police Brotherhood Home Page';
@@ -56,6 +58,9 @@ function public_main( $page = null, $nav_to = null, $img_dir = null, $page_no = 
 }
 
 function public_blueMass( $page = null, $dir_name1 = null, $dir_name2 = null, $page_no = null ) {
+    $this->load->module('lib');
+    $this->load->module('nav_menu');
+
     // get images from directory`
     $imgDir   = 'public/images/'.$dir_name1.'/'.$dir_name2;
 
@@ -67,8 +72,8 @@ function public_blueMass( $page = null, $dir_name1 = null, $dir_name2 = null, $p
     $data['bm_pages'] = $this->lib->image_pagination( $imgDir );
     $data['page']     = $page == null ? 'main' : $page;
     $data['dir_name1']= $dir_name1;
-    $data['img_dir']  = $dir_name2;    
-    $data['nav_to']   = !isset($dir_name2) ? 'Blue Mass ' : 'Blue Mass '.substr( $dir_name2, 2);    
+    $data['img_dir']  = $dir_name2;
+    $data['nav_to']   = !isset($dir_name2) ? 'Blue Mass ' : 'Blue Mass '.substr( $dir_name2, 2);
     $data['contents'] = $page == null ? 'main' : $page;
 
     $this->load->view('public/html_master_view', $data);
@@ -76,12 +81,14 @@ function public_blueMass( $page = null, $dir_name1 = null, $dir_name2 = null, $p
 
 function public_jqm() {
     echo "<h1>public jqm page.............. </h1>";
-   // $load->load->view['public_jqm', $data];
+   // $load->load->view('public_jqm', $data);
 }
 
-function admin() {
-    // echo "<h1>public admin page.............. </h1>";
-    $this->load->view('admin/index');
+function admin( $data = array() ) {
+    $this->load->module('site_security');
+    $this->site_security->_make_sure_is_admin();
+  
+    $this->load->view('admin/admin', $data);
 }
 
 
